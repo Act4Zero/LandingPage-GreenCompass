7d945fb400f7cc482e05ec84d5bfb4374e6db4ad
Stoyan Korudzhiev <skorudzhiev@gmail.com>
2024-12-11 11:14:11 +0200

    [Content] Replace and reduce the Footer section content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,12 @@ import Section from "components/Section";
 import TextField from "components/TextField";
 import Button from "components/Button";
 import newsletter from "util/newsletter";
+import { useTranslation } from "react-i18next";
 
 function Footer(props) {
   const [subscribed, setSubscribed] = useState(false);
   const { handleSubmit, register, errors } = useForm();
+  const { t } = useTranslation();
 
   const onSubmit = ({ email }) => {
     setSubscribed(true);
@@ -32,15 +34,13 @@ function Footer(props) {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
           <div>
             <h4 className="text-sm uppercase font-semibold tracking-wider text-gray-400">
-              Product
+              {t("footer.product.title")}
             </h4>
             <nav className="flex flex-col space-y-3 mt-6">
               {[
-                { url: "/pricing", name: "Pricing" },
-                { url: "/faq", name: "FAQ" },
-                { url: "/", name: "Features" },
-                { url: "/", name: "Solutions" },
-                { url: "/", name: "Resources" },
+                { url: "/faq", name: t("footer.product.faq") },
+                { url: "/", name: t("footer.product.features") },
+                { url: "/", name: t("footer.product.timeline") },
               ].map((link, index) => (
                 <Link href={link.url} key={index}>
                   <a className="font-medium text-gray-600 hover:text-gray-500">
@@ -52,15 +52,13 @@ function Footer(props) {
           </div>
           <div>
             <h4 className="text-sm uppercase font-semibold tracking-wider text-gray-400">
-              Company
+              {t("footer.company.title")}
             </h4>
             <nav className="flex flex-col space-y-3 mt-6">
               {[
-                { url: "/about", name: "About" },
-                { url: "/contact", name: "Contact" },
-                { url: "/", name: "Blog" },
-                { url: "/legal/terms-of-service", name: "Terms of Service" },
-                { url: "/legal/privacy-policy", name: "Privacy Policy" },
+                { url: "/about", name: t("footer.company.about") },
+                { url: "/contact", name: t("footer.company.contact") },
+                { url: "/legal/privacy-policy", name: t("footer.company.privacy") },
               ].map((link, index) => (
                 <Link href={link.url} key={index}>
                   <a className="font-medium text-gray-600 hover:text-gray-500">
@@ -72,7 +70,7 @@ function Footer(props) {
           </div>
           <div>
             <h4 className="text-sm uppercase font-semibold tracking-wider text-gray-400">
-              Social
+              {t("footer.social.title")}
             </h4>
             <div className="mt-6 flex flex-row space-x-4">
               {[
@@ -94,12 +92,6 @@ function Footer(props) {
                     <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z" />
                   ),
                 },
-                {
-                  url: "https://github.com/",
-                  icon: (
-                    <path d="M12 0C5.374 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0112 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.3 24 12c0-6.627-5.373-12-12-12z" />
-                  ),
-                },
               ].map((link, index) => (
                 <Link href={link.url} key={index}>
                   <a
@@ -119,7 +111,7 @@ function Footer(props) {
               ))}
             </div>
             <h4 className="mt-10 text-sm uppercase font-semibold tracking-wider text-gray-400">
-              Join Our Newsletter
+              {t("footer.form.title")}
             </h4>
 
             {subscribed === true && (
@@ -136,16 +128,16 @@ function Footer(props) {
                     type="email"
                     id="email"
                     name="email"
-                    placeholder="Email"
+                    placeholder={t("footer.form.placeholder")}
                     error={errors.email}
                     size="sm"
                     inputRef={register({
-                      required: "Please enter an email address",
+                      required: t("form.required"),
                     })}
                   />
                 </div>
                 <Button type="submit" size="sm">
-                  Subscribe
+                  {t("footer.form.button")}
                 </Button>
               </form>
             )}
