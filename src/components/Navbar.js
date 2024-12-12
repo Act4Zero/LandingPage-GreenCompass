ff0605e67598e478790452a0587ccad99dd645a9
Stoyan Korudzhiev <skorudzhiev@gmail.com>
2024-12-12 09:49:39 +0200

    [Content] Reduce Navbar items and replace logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,18 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import {
-  ChevronDownIcon,
-  CodeBracketIcon,
-  SwatchIcon,
-  PuzzlePieceIcon,
-  BeakerIcon,
-  DocumentIcon,
-  LifebuoyIcon,
-  ChatBubbleLeftEllipsisIcon,
   Bars3Icon,
 } from "@heroicons/react/24/solid";
-import Section from "components/Section";
-import FeatureIcon from "components/FeatureIcon";
+import Section from "components/Section"
 import Button from "components/Button";
+import Image from "next/image";
 
 function Navbar(props) {
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
 
   const classes = {
     navLink:
-      "font-semibold inline-flex items-center space-x-1 h-8 px-4 group-hover:text-blue-500 py-6",
+      "font-semibold inline-flex items-center space-x-1 h-8 px-4 group-hover:text-green-800 py-6",
     navLinkIcon:
       "opacity-50 transform transition duration-200 ease-out group-hover:rotate-180 inline-block w-4 h-4",
     dropdown: {
@@ -34,7 +26,7 @@ function Navbar(props) {
         "bg-white shadow-xl ring-1 ring-black ring-opacity-5 rounded-lg overflow-hidden",
       title:
         "text-xs uppercase font-semibold tracking-wider text-blue-800 mb-5",
-      link: "text-gray-600 hover:text-blue-600 font-medium text-sm flex items-center space-x-2",
+      link: "text-gray-600 hover:text-green-800 font-medium text-sm flex items-center space-x-2",
       icon: "opacity-25 inline-block w-5 h-5",
       feature:
         "p-3 rounded-xl flex items-center space-x-4 text-gray-600 font-medium text-sm",
@@ -58,219 +50,24 @@ function Navbar(props) {
       <div className="container py-4">
         <div className="flex justify-between">
           <Link href="/">
-            <a className="inline-flex items-center space-x-2 font-bold text-lg tracking-wide text-blue-600 hover:text-blue-400">
-              <svg
-                className="opacity-75 hi-outline hi-cube-transparent inline-block w-6 h-6"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5"
-                />
-              </svg>
-              <span>Company</span>
+            <a className="inline-flex items-center space-x-2 font-bold text-lg tracking-wide text-black hover:text-green-800">
+              <Image
+                src="/images/GCLogo-no-bg.png"
+                alt="Green Compass Logo"
+                width={48}
+                height={48}
+              />
+              <span>Green Compass</span>
             </a>
           </Link>
           <div className="flex items-center ml-auto space-x-1 lg:space-x-4">
             <ul className="hidden lg:flex items-center ml-auto">
-              <li className="relative group">
-                <span className={`${classes.navLink}`} href="">
-                  <span>Solutions</span>
-                  <ChevronDownIcon className={classes.navLinkIcon} />
-                </span>
-                <div
-                  className={`${classes.dropdown.base} ${classes.dropdown.large} ${classes.dropdown.center}`}
-                >
-                  <div className={`${classes.dropdown.inner}`}>
-                    <div className="p-3">
-                      <div className="px-3 pt-3">
-                        <h4 className={`${classes.dropdown.title}`}>
-                          Featured Products
-                        </h4>
-                      </div>
-                      <nav className="flex flex-col space-y-1">
-                        <Link href="/">
-                          <a
-                            className={`hover:text-blue-600 hover:bg-blue-50 ${classes.dropdown.feature}`}
-                          >
-                            <div>
-                              <FeatureIcon color="blue">
-                                <CodeBracketIcon />
-                              </FeatureIcon>
-                            </div>
-                            <div>
-                              <h5 className={`${classes.dropdown.featureName}`}>
-                                HTML Editor
-                              </h5>
-                              <p
-                                className={`${classes.dropdown.featureDescription}`}
-                              >
-                                Write and manipulate your markup directly in
-                                your browser
-                              </p>
-                            </div>
-                          </a>
-                        </Link>
-                        <Link href="/">
-                          <a
-                            className={`hover:text-emerald-600 hover:bg-emerald-50 ${classes.dropdown.feature}`}
-                          >
-                            <div>
-                              <FeatureIcon color="emerald">
-                                <SwatchIcon />
-                              </FeatureIcon>
-                            </div>
-                            <div>
-                              <h5 className={`${classes.dropdown.featureName}`}>
-                                CSS Editor
-                              </h5>
-                              <p
-                                className={`${classes.dropdown.featureDescription}`}
-                              >
-                                Style your markup code with smart tools
-                                supporting Sass
-                              </p>
-                            </div>
-                          </a>
-                        </Link>
-                        <Link href="/">
-                          <a
-                            className={`hover:text-orange-600 hover:bg-orange-50 ${classes.dropdown.feature}`}
-                          >
-                            <div>
-                              <FeatureIcon color="orange">
-                                <PuzzlePieceIcon />
-                              </FeatureIcon>
-                            </div>
-                            <div>
-                              <h5 className={`${classes.dropdown.featureName}`}>
-                                Web Page Builder
-                              </h5>
-                              <p
-                                className={`${classes.dropdown.featureDescription}`}
-                              >
-                                Explore all available components and build your
-                                website
-                              </p>
-                            </div>
-                          </a>
-                        </Link>
-                      </nav>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li className="relative group">
-                <span className={`${classes.navLink}`} href="">
-                  <span>Resources</span>
-                  <ChevronDownIcon className={classes.navLinkIcon} />
-                </span>
-                <div
-                  className={`${classes.dropdown.base} ${classes.dropdown.large} ${classes.dropdown.center}`}
-                >
-                  <div className={`${classes.dropdown.inner}`}>
-                    <div className="grid grid-cols-2">
-                      <div className="p-6 space-y-6">
-                        <h4 className={`${classes.dropdown.title}`}>Learn</h4>
-                        <nav className="flex flex-col space-y-3">
-                          <Link href="/">
-                            <a className={`${classes.dropdown.link}`}>
-                              Resource Center
-                            </a>
-                          </Link>
-                          <Link href="/">
-                            <a className={`${classes.dropdown.link}`}>
-                              Developer
-                            </a>
-                          </Link>
-                          <Link href="/">
-                            <a className={`${classes.dropdown.link}`}>
-                              User Guides
-                            </a>
-                          </Link>
-                        </nav>
-                      </div>
-                      <div className="p-6 space-y-6">
-                        <h4 className={`${classes.dropdown.title}`}>Links</h4>
-                        <nav className="flex flex-col space-y-3">
-                          <Link href="/">
-                            <a className={`${classes.dropdown.link}`}>
-                              Starters
-                            </a>
-                          </Link>
-                          <Link href="/">
-                            <a className={`${classes.dropdown.link}`}>
-                              Video Library
-                            </a>
-                          </Link>
-                          <Link href="/">
-                            <a className={`${classes.dropdown.link}`}>
-                              Tutorials
-                            </a>
-                          </Link>
-                        </nav>
-                      </div>
-                    </div>
-                    <div className="grid grid-cols-2 bg-gray-50">
-                      <nav className="p-6 flex flex-col space-y-4">
-                        <Link href="/">
-                          <a className={`${classes.dropdown.link}`}>
-                            <BeakerIcon className={classes.dropdown.icon} />
-                            <span>API Reference</span>
-                          </a>
-                        </Link>
-                        <Link href="/">
-                          <a className={`${classes.dropdown.link}`}>
-                            <DocumentIcon className={classes.dropdown.icon} />
-                            <span>Documentation</span>
-                          </a>
-                        </Link>
-                      </nav>
-                      <nav className="p-6 flex flex-col space-y-4">
-                        <Link href="/">
-                          <a className={`${classes.dropdown.link}`}>
-                            <LifebuoyIcon className={classes.dropdown.icon} />
-                            <span>Support</span>
-                          </a>
-                        </Link>
-                        <Link href="/">
-                          <a className={`${classes.dropdown.link}`}>
-                            <ChatBubbleLeftEllipsisIcon
-                              className={classes.dropdown.icon}
-                            />
-                            <span>Support</span>
-                          </a>
-                        </Link>
-                      </nav>
-                    </div>
-                  </div>
-                </div>
-              </li>
               <li className="relative group">
                 <Link href="/about">
                   <a className={`${classes.navLink}`}>
                     <span>About</span>
                   </a>
                 </Link>
-              </li>
-              <li className="relative group">
-                <Link href="/pricing">
-                  <a className={`${classes.navLink}`}>
-                    <span>Pricing</span>
-                  </a>
-                </Link>
-              </li>
-              <li className="relative group">
-                <a
-                  className={`${classes.navLink}`}
-                  href="https://app.mysite.com/auth/signin"
-                >
-                  <span>Sign in</span>
-                </a>
               </li>
             </ul>
             <div className="flex lg:hidden items-center justify-center">
@@ -285,100 +82,11 @@ function Navbar(props) {
         </div>
         <div className={"lg:hidden" + (!mobileNavOpen ? " hidden" : "")}>
           <div className="grid grid-cols-1 sm:grid-cols-3 border rounded-xl my-5">
-            <div className="col-span-2 p-3 space-y-6">
-              <div className="px-3 pt-3">
-                <h4 className={`${classes.dropdown.title}`}>
-                  Featured Products
-                </h4>
-              </div>
-              <nav className="flex flex-col space-y-1">
-                <Link href="/">
-                  <a
-                    className={`hover:text-blue-600 hover:bg-blue-50 ${classes.dropdown.feature}`}
-                  >
-                    <div>
-                      <FeatureIcon color="blue">
-                        <CodeBracketIcon />
-                      </FeatureIcon>
-                    </div>
-                    <div>
-                      <h5 className={`${classes.dropdown.featureName}`}>
-                        HTML Editor
-                      </h5>
-                      <p className={`${classes.dropdown.featureDescription}`}>
-                        Write and manipulate your markup directly in your
-                        browser
-                      </p>
-                    </div>
-                  </a>
-                </Link>
-                <Link href="/">
-                  <a
-                    className={`hover:text-emerald-600 hover:bg-emerald-50 ${classes.dropdown.feature}`}
-                  >
-                    <div>
-                      <FeatureIcon color="emerald">
-                        <SwatchIcon />
-                      </FeatureIcon>
-                    </div>
-                    <div>
-                      <h5 className={`${classes.dropdown.featureName}`}>
-                        CSS Editor
-                      </h5>
-                      <p className={`${classes.dropdown.featureDescription}`}>
-                        Style your markup code with smart tools supporting Sass
-                      </p>
-                    </div>
-                  </a>
-                </Link>
-                <Link href="/">
-                  <a
-                    className={`hover:text-orange-600 hover:bg-orange-50 ${classes.dropdown.feature}`}
-                  >
-                    <div>
-                      <FeatureIcon color="orange">
-                        <PuzzlePieceIcon />
-                      </FeatureIcon>
-                    </div>
-                    <div>
-                      <h5 className={`${classes.dropdown.featureName}`}>
-                        Web Page Builder
-                      </h5>
-                      <p className={`${classes.dropdown.featureDescription}`}>
-                        Explore all available components and build your website
-                      </p>
-                    </div>
-                  </a>
-                </Link>
-              </nav>
-            </div>
             <div className="p-6 space-y-6">
-              <h4 className={`${classes.dropdown.title}`}>Learn</h4>
-              <nav className="flex flex-col space-y-3">
-                <Link href="/">
-                  <a className={`${classes.dropdown.link}`}>Resource Center</a>
-                </Link>
-                <Link href="/">
-                  <a className={`${classes.dropdown.link}`}>Developer</a>
-                </Link>
-                <Link href="/">
-                  <a className={`${classes.dropdown.link}`}>User Guides</a>
-                </Link>
-              </nav>
-              <h4 className={`${classes.dropdown.title}`}>More</h4>
               <nav className="flex flex-col space-y-3">
                 <Link href="/about">
                   <a className={`${classes.dropdown.link}`}>About</a>
                 </Link>
-                <Link href="/pricing">
-                  <a className={`${classes.dropdown.link}`}>Pricing</a>
-                </Link>
-                <a
-                  className={`${classes.dropdown.link}`}
-                  href="https://app.mysite.com/auth/signin"
-                >
-                  Sign in
-                </a>
               </nav>
             </div>
           </div>
