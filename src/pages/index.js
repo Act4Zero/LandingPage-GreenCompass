1f227667e599ddac5fd9e6057e319c6816583163
Stoyan Korudzhiev <skorudzhiev@gmail.com>
2024-12-12 10:16:52 +0200

    fix build issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './_i18n';
+import '../util/i18n';
 import { useTranslation } from 'react-i18next';
 import Meta from "components/Meta";
 import Navbar from "components/Navbar";
