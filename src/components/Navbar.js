91e39589822d7e5fb1f78edefb71f5bc4ea5c15d
Stoyan Korudzhiev <skorudzhiev@gmail.com>
2024-12-15 09:34:48 +0200

    [Theme] Update Navbar colors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import {
-  Bars3Icon,
-} from "@heroicons/react/24/solid";
-import Section from "components/Section"
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import Section from "components/Section";
 import Button from "components/Button";
 import Image from "next/image";
 
@@ -12,45 +10,28 @@ function Navbar(props) {
 
   const classes = {
     navLink:
-      "font-semibold inline-flex items-center space-x-1 h-8 px-4 group-hover:text-green-800 py-6",
+      "font-semibold inline-flex items-center space-x-1 h-8 px-4 py-6 group-hover:text-green-dark transition duration-200",
     navLinkIcon:
       "opacity-50 transform transition duration-200 ease-out group-hover:rotate-180 inline-block w-4 h-4",
     dropdown: {
       base: "absolute top-19 pt-1 z-10 invisible group-hover:visible transform transition duration-800 ease-in opacity-0 group-hover:opacity-100",
-      left: "right-0",
-      center: "left-1/2 -translate-x-1/2",
-      right: "left-0",
-      normal: "w-48",
-      large: "w-96",
       inner:
         "bg-white shadow-xl ring-1 ring-black ring-opacity-5 rounded-lg overflow-hidden",
-      title:
-        "text-xs uppercase font-semibold tracking-wider text-blue-800 mb-5",
-      link: "text-gray-600 hover:text-green-800 font-medium text-sm flex items-center space-x-2",
-      icon: "opacity-25 inline-block w-5 h-5",
-      feature:
-        "p-3 rounded-xl flex items-center space-x-4 text-gray-600 font-medium text-sm",
-      featureName: "font-semibold mb-1",
-      featureDescription: "opacity-75",
+      link: "text-green-dark hover:text-green-darker font-medium text-sm flex items-center space-x-2",
     },
     accountDropdown: {
-      base: "absolute right-0 origin-top-right mt-2 w-48 z-10",
       inner:
-        "bg-white ring-1 ring-black ring-opacity-5 rounded divide-y divide-gray-100 shadow-xl rounded",
-      link: "flex items-center space-x-2 rounded py-2 px-3 text-sm font-medium text-gray-600 hover:bg-gray-100 hover:text-gray-700",
-      linkActive: "text-gray-700 bg-gray-100",
-      linkInactive:
-        "text-gray-600 hover:bg-gray-100 hover:text-gray-700 focus:outline-none focus:bg-gray-100 focus:text-gray-700",
-      icon: "opacity-50 inline-block w-5 h-5",
+        "bg-white ring-1 ring-black ring-opacity-5 rounded divide-y divide-gray-100 shadow-xl",
+      link: "flex items-center space-x-2 rounded py-2 px-3 text-sm font-medium text-green-dark hover:bg-green-light hover:text-green-darker",
     },
   };
 
   return (
-    <Section bgColor={props.bgColor}>
+    <Section bgColor="bg-white">
       <div className="container py-4">
         <div className="flex justify-between">
           <Link href="/">
-            <a className="inline-flex items-center space-x-2 font-bold text-lg tracking-wide text-black hover:text-green-800">
+            <a className="inline-flex items-center space-x-2 font-bold text-lg tracking-wide text-green-dark hover:text-green">
               <Image
                 src="/images/GCLogo-no-bg.png"
                 alt="Green Compass Logo"
@@ -64,7 +45,7 @@ function Navbar(props) {
             <ul className="hidden lg:flex items-center ml-auto">
               <li className="relative group">
                 <Link href="/about">
-                  <a className={`${classes.navLink}`}>
+                  <a className={classes.navLink}>
                     <span>About</span>
                   </a>
                 </Link>
@@ -75,17 +56,19 @@ function Navbar(props) {
                 variant="simple"
                 size="sm"
                 onClick={() => setMobileNavOpen(!mobileNavOpen)}
-                endIcon={<Bars3Icon className="inline-block w-5 h-5" />}
+                endIcon={
+                  <Bars3Icon className="inline-block w-5 h-5 text-green-dark" />
+                }
               />
             </div>
           </div>
         </div>
         <div className={"lg:hidden" + (!mobileNavOpen ? " hidden" : "")}>
-          <div className="grid grid-cols-1 sm:grid-cols-3 border rounded-xl my-5">
+          <div className="grid grid-cols-1 sm:grid-cols-3 border rounded-xl my-5 bg-green-lightest">
             <div className="p-6 space-y-6">
               <nav className="flex flex-col space-y-3">
                 <Link href="/about">
-                  <a className={`${classes.dropdown.link}`}>About</a>
+                  <a className={classes.dropdown.link}>About</a>
                 </Link>
               </nav>
             </div>
