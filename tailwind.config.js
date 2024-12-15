bdbdac1993b46811ae493bef92b8bbda582d0ad2
Stoyan Korudzhiev <skorudzhiev@gmail.com>
2024-12-15 09:22:21 +0200

    [Theme] Add default theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        green: {
+          lightest: "#EAFFEA",
+          light: "#B5F1BF",
+          DEFAULT: "#7FB88B",
+          dark: "#4B8359",
+          darkest: "#18512C",
+        },
+        brown: {
+          DEFAULT: "#87521B",
+        },
+        lightBg: "#FFEEE0",
+        accentLight: "#FFDCC1",
+      },
+    },
     container: {
       center: true,
       padding: "2rem",
