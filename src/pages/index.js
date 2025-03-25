1ea2de3e56b49a04bdf692f3f9947bb0ac68b725
Stoyan Korudzhiev <skorudzhiev@gmail.com>
2025-03-25 16:54:27 +0200

    Reduce the content size component

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,12 +51,8 @@ function IndexPage(props) {
       />
       <ContentSection
         id="content"
-        title={t("index.content.title")}
         paragraphs={[
-          t("index.content.paragraph1"),
-          t("index.content.paragraph2"),
-          t("index.content.paragraph3"),
-          t("index.content.paragraph4"),
+          t("index.content.paragraph1")
         ]}
         showButton={true}
         buttonText={t("index.content.cta")}
